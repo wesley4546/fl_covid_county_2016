ed5265284f9209ad681d04791971b887b3f22ec3
ejohnson0916 <ean.m.johnson1@gmail.com>
2020-06-19 13:52:22 -0400

    Started process of cleaning data

--- R/covid_by_county_florida_2016.R
+++ R/covid_by_county_florida_2016.R
@@ -1,22 +1,39 @@
 library(janitor)
 library(ggplot2)
 library(dplyr)
+library(tidyverse)
+install.packages("tidyverse")
 
+updateR()
 
 # Reading Data ------------------------------------------------------------
-
+#Florida election results
 data <- read.csv(
-  here::here("data","florida_county_election_results_2016.csv")
+  here::here("data","florida_county_election_results_2016.csv", stringsAsFactors = FALSE)
   )
 
-
-
-
+#
 covid_cases_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 covid_deaths_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
 
+cases <- read.csv(url(covid_cases_url))
 # Cleaning Data -----------------------------------------------------------
-#Test commit
+
+str(cases)
+
+
+cases_data <- 
+  cases %>%
+  filter(Country_Region == "US") %>%
+  select(Province_State, X1.22.20:ncol(cases)) %>% 
+  rename(state= Province_State)
+  
+
+
+str(cases_data)  
+
+
+
 
 
 # tutorial on joins in R https://r4ds.had.co.nz/relational-data.html#mutating-joins

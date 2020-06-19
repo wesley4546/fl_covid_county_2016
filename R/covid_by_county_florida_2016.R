c6eb07660dd1dbbe267e6277a233f30992267096
ejohnson0916 <ean.m.johnson1@gmail.com>
2020-06-19 09:10:03 -0400

    Test Commit

--- R/covid_by_county_florida_2016.R
+++ R/covid_by_county_florida_2016.R
@@ -5,16 +5,19 @@ library(dplyr)
 
 # Reading Data ------------------------------------------------------------
 
-election_data <- read.csv(
+data <- read.csv(
   here::here("data","florida_county_election_results_2016.csv")
-  ) %>% 
-  clean_names()
+  )
+
+
 
 
 covid_cases_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 covid_deaths_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
 
 # Cleaning Data -----------------------------------------------------------
+#Test commit
+
 
 # tutorial on joins in R https://r4ds.had.co.nz/relational-data.html#mutating-joins
 # Use sometype of joining 

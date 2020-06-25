33d07d59c97e2adc21e820df906a9832cc94f7af
Wesley G <56905659+wesley4546@users.noreply.github.com>
2020-06-25 17:13:34 -0400

    6/25/2020 meeting

--- R/covid_by_county_florida_2016.R
+++ R/covid_by_county_florida_2016.R
@@ -7,9 +7,13 @@ library(tidyverse)
 
 # Reading Data ------------------------------------------------------------
 
-
 #Florida election results
-election_data <- florida_county_election_results_2016 
+election_data <- 
+  read.csv(here::here("data","florida_county_election_results_2016.csv")) 
+
+pop_data_raw <- 
+  read.csv(here::here("data","population_county_data.csv")) %>% 
+  rename(Rank = "ï..Rank")
 
 #WHO COVID Date
 covid_cases_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
@@ -18,7 +22,6 @@ covid_deaths_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/m
 #Read Data to CSV
 deaths <- read.csv(url(covid_deaths_url), stringsAsFactors = FALSE)
 cases <- read.csv(url(covid_cases_url), stringsAsFactors = FALSE)
-
 
 # Cleaning Cases Data -----------------------------------------------------
 
@@ -41,7 +44,6 @@ cases$Date <- gsub("X", "", cases$Date)
 # set Date column to Date 
 cases$Date <- as.Date(cases$Date, format ="%m.%d.%y")
 
-
 # Cleaning Deaths Data ----------------------------------------------------
 
 deaths <- 
@@ -63,14 +65,17 @@ deaths$Date <- gsub("X", "", deaths$Date)
 #Change data type to Date of Date column
 deaths$Date <- as.Date(deaths$Date, format = "%m.%d.%y")
 
+# Cleaning Population Data ------------------------------------------------
+
+pop_data_clean <-
+  pop_data_raw %>% 
+  mutate(County = gsub(" County", "", County))
 
 # Joining Election Data with COVID Data -----------------------------------
-str(election_data)
 
 election_data <-
   election_data %>%
-  rename(County = "Vote by county")
-
+  rename(County = "ï..Vote.by.county")
 
 main_data_cases <- 
   cases %>% 
@@ -81,19 +86,51 @@ main_data_deaths <-
   deaths %>%
   left_join(election_data, by = "County")
 
-
 # tutorial on joins in R https://r4ds.had.co.nz/relational-data.html#mutating-joins
 # GitHub to my previous cleaning of the same data : https://github.com/wesley4546/covidstate/tree/master/R
 
+# Normalizing Counts by Pop -----------------------------------------------
+
+
+norm_case_data <- 
+  main_data_cases %>% 
+  inner_join(pop_data_clean, by = "County") %>% 
+  mutate(Population = as.integer(gsub(",","", Population))) %>% #Removes `,` and makes int
+  mutate(normalized_cases = case_count/Population) %>% 
+  clean_names()
+
 # Graphing Data -----------------------------------------------------------
-ggplot(main_data, aes(x=Date)) +
-  geom_line(y=case_count, group = county)
 
+# graphing data from picture of historgram and bar chart look at ggpubr::ggscatterhist()
 
+# Cases
+
+graph_data_cases <-
+  main_data_cases %>% 
+  mutate(Party = as.factor(Party)) %>% 
+  filter(case_count > 0)
+
+norm_case_data <- 
+  norm_case_data %>% 
+  filter(normalized_cases > 0) 
+
+norm_case_data$party <- factor(
+  norm_case_data$party, levels = c("republican", "democrat")
+)
+  
+norm_case_data %>% 
+  ggplot(aes(x=date, y = normalized_cases, group = county, color=party)) +
+  geom_line() +
+  geom_point()
 
 # ggplot; color by party
 
+# Notes -------------------------------------------------------------------
 
-
+# add pretty labels (x, y , title, subtitle)
+# experiment with different types of graphs (x and y axis)
+# get a story together about the jumps
+# Keep track of where you get the data
+# population data: https://www.florida-demographics.com/counties_by_population
 
 
